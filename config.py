bbf3f43b5437e1bc7d0b814eeec214255a1d1390
이용민 <bacaren21@naver.com>
2026-01-14 17:56:54 +0900

    병원 순위 리턴 변경

--- config.py
+++ config.py
@@ -6,9 +6,13 @@ load_dotenv()
 SERVICE_KEY = os.getenv("SERVICE_KEY")
 API_URL = os.getenv("API_URL")
 LLM_API_KEY = os.getenv("LLM_API_KEY")
+KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY")
 
 if not SERVICE_KEY:
     raise RuntimeError("SERVICE_KEY is not set in .env")
 
 if not API_URL:
     raise RuntimeError("API_URL is not set in .env")
+
+if not KAKAO_REST_API_KEY:
+    raise RuntimeError("KAKAO_REST_API_KEY is not set in .env")
